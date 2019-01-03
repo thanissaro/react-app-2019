9c1aed4ef4d4a3a5a82c99db4492a4a129637ea6
Kevin Luang-orn <kluangorn@gmail.com>
2019-01-03 01:24:47 -0800

    wip

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+const person = {
+  talk() {
+    console.log("this", this);
+  }
+};
 
-const element = <h1>Hello World</h1>;
-console.log(element);
+person.talk();
